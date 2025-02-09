81953edc5b16b96d279945acf20923cb9f249ed3
Liza-2008 <liza0525.becse24@chitkara.edu.in>
2025-02-09 21:17:55 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -2,8 +2,6 @@
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-
-    // Triangle validity condition: Sum of any two sides must be greater than the third side
     if ((a + b > c) && (a + c > b) && (b + c > a)) {
         printf("Valid\n");
     } else {
