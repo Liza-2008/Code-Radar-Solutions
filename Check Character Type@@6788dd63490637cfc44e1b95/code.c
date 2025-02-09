f41a6244c7400a29603e9e9d6af2076cef900cf3
Liza-2008 <liza0525.becse24@chitkara.edu.in>
2025-02-09 21:51:33 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
-int main(){
-    char a;
-    scanf("%c",&a);
-    if ((a>='a' && a<='z')||(a>='A' && a<='Z')){
-        if(a=='a'||a=='e'||a=='i'||a=='o'||a='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
-            printf("Vowel\n");
-        }else{
-            printf("Constant\n");
-        }
-    }else if(a>='0' && a<='9'){
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+
+    // Check if it's a digit
+    if (ch >= '0' && ch <= '9') {
         printf("Digit\n");
-    }else{
-        printf("Special Character");
     }
+    // Check if it's an uppercase or lowercase vowel
+    else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+             ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("Vowel\n");
+    }
+    // Check if it's an uppercase or lowercase letter (consonant)
+    else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        printf("Consonant\n");
+    }
+    // If it's none of the above, it's a special character
+    else {
+        printf("Special Character\n");
+    }
+
     return 0;
 }
