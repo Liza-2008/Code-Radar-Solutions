a138af98f836030e0e80a6b0d976c7a4f0082bea
Liza-2008 <liza0525.becse24@chitkara.edu.in>
2025-02-09 19:51:23 +0530

    Solution to Count Trailing Zeros in a Binary Number

--- Count Trailing Zeros in a Binary Number@@67882a731b4392d8fc1577f1/code.c
+++ Count Trailing Zeros in a Binary Number@@67882a731b4392d8fc1577f1/code.c
@@ -1,42 +1,42 @@
-// #include <stdio.h>
-// int countTrailingZeros(unsigned int num)
-// {
-//     if (num==o) return 32;
-//     int count=0;
-//     while((num&1)==0)
-//     {
-//         count++;
-//         num>>=1;
-//     }
-//     return count;
-// }
-// int main()
-// {
-//     unsigned int num;
-//     scanf("%u",&num);
-//     printf("%d\n",countTrailingZeros(num));
-//     return 0;
-// }
-
-
 #include <stdio.h>
-
-int countTrailingZeros(unsigned int num) {
-    if (num == 0) return 32; // If number is 0, all bits are 0 (assuming 32-bit integer)
-    
-    int count = 0;
-    while ((num & 1) == 0) { // Check least significant bit (LSB)
+int countTrailingZeros(unsigned int num)
+{
+    if (num==o) return 32;
+    int count=0;
+    while((num&1)==0)
+    {
         count++;
-        num >>= 1; // Right shift to check the next bit
+        num>>=1;
     }
-    
     return count;
 }
-
-int main() {
+int main()
+{
     unsigned int num;
-    scanf("%u", &num);
-    
+    scanf("%u",&num);
     printf("%d\n", countTrailingZeros(num));
     return 0;
 }
+
+
+// #include <stdio.h>
+
+// int countTrailingZeros(unsigned int num) {
+//     if (num == 0) return 32; // If number is 0, all bits are 0 (assuming 32-bit integer)
+    
+//     int count = 0;
+//     while ((num & 1) == 0) { // Check least significant bit (LSB)
+//         count++;
+//         num >>= 1; // Right shift to check the next bit
+//     }
+    
+//     return count;
+// }
+
+// int main() {
+//     unsigned int num;
+//     scanf("%u", &num);
+    
+//     printf("%d\n", countTrailingZeros(num));
+//     return 0;
+// }
